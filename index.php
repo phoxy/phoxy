cb9b111392dded9632738e937355c81b6fcccb89
Kirill Berezin <enelar@develop-project.ru>
2013-05-21 22:07:52 +0400

    Require php 5.4 or newer. Remove db connect

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
-error_reporting(E_ALL); ini_set('display_errors', '1');
-
-include_once('connect.php');
+if (strnatcmp(phpversion(),'5.4') < 0)
+  exit("PHP 5.4 or newer is required");
 
 if (!count($_GET))
 {
