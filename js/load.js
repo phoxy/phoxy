2737420c9316e6dd33bb2717b74f6741e9def5e8
Kirill Berezin <enelar@develop-project.ru>
2013-05-21 20:18:49 +0400

    phoxy as js claa

--- js/load.js
+++ js/load.js
@@ -9,45 +9,48 @@ require([
     "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js",
     "libs/jquery.form"]);
     
-    //SimpleApiRequest("api/main");
   }
   );
 
-function ApiAnswer( answer, callback )
+var phoxy =
 {
-  if (answer.reset)
-    location.reload(answer.reset);
-  if (answer.error)
-  {
-    alert(answer.error);
-    return;
-  }
-  var source = "ejs/" + answer.design;
-  if (answer.result)
-    new EJS({url: source}).update(answer.result, answer.data);
-  else
-    $('body').append(new EJS({url: source}).render(answer.data));
-  if (answer.script)
-  {
-    require(answer.script,
-      function()
+  ApiAnswer : function ApiAnswer( answer, callback )
+    {
+      if (answer.reset)
+        location.reload(answer.reset);
+      if (answer.error)
       {
-        if (answer.routeline)
-          window[answer.routeline](answer.data);
-        if (callback)
-          callback(answer.data);
+        alert(answer.error);
+        return;
       }
-    );  
-  }
-  else
-    if (callback)
-      callback(answer);
-}
+      var source = "ejs/" + answer.design;
 
-function SimpleApiRequest( url )
-{
-  $(function()
-{
-  $.getJSON(url, function(data) { ApiAnswer(data); });
-});
+      if (answer.result)
+        new EJS({url: source}).update(answer.result, answer.data);
+      else
+        $('body').append(new EJS({url: source}).render(answer.data));
+      if (answer.script)
+      {
+        require(answer.script,
+          function()
+          {
+          if (answer.routeline)
+            window[answer.routeline](answer.data);
+          if (callback)
+            callback(answer.data);
+          }
+        );  
+      }
+      else
+        if (callback)
+          callback(answer);
+    }
+  ,
+  SimpleApiRequest : function( url )
+    {
+      $(function()
+      {
+        $.getJSON(url, function(data) { ApiAnswer(data); });
+      });
+    }
 }
