21e61cc4c965b1553f48d2f23472cd16a649b868
Kirill Berezin <enelar@develop-project.ru>
2013-05-21 20:21:03 +0400

    load.js => phoxy.js, libs to near location

---
+++ phoxy.js
@@ -0,0 +1,55 @@
+require([
+  "libs/text", // part of require js
+  "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js",
+  "libs/ejs_production"
+  ],
+  function(until)
+  {
+    require([
+    "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js",
+    "libs/jquery.form"]);
+  }
+  );
+
+var phoxy =
+{
+  ApiAnswer : function ApiAnswer( answer, callback )
+    {
+      if (answer.reset)
+        location.reload(answer.reset);
+      if (answer.error)
+      {
+        alert(answer.error);
+        return;
+      }
+      var source = "ejs/" + answer.design;
+
+      if (answer.result)
+        new EJS({url: source}).update(answer.result, answer.data);
+      else
+        $('body').append(new EJS({url: source}).render(answer.data));
+      if (answer.script)
+      {
+        require(answer.script,
+          function()
+          {
+          if (answer.routeline)
+            window[answer.routeline](answer.data);
+          if (callback)
+            callback(answer.data);
+          }
+        );  
+      }
+      else
+        if (callback)
+          callback(answer);
+    }
+  ,
+  SimpleApiRequest : function( url )
+    {
+      $(function()
+      {
+        $.getJSON(url, function(data) { ApiAnswer(data); });
+      });
+    }
+}
