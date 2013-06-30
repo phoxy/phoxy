a38c9ae554e11a1461aafc69da716d88de7f3939
Kirill Berezin <enelar@develop-project.ru>
2013-06-30 23:12:34 +0400

    Speed up hash instruction load

--- phoxy.js
+++ phoxy.js
@@ -11,18 +11,40 @@ require([
       $('script[phoxy]').each(function()
       {
         phoxy.SimpleApiRequest($(this).attr("phoxy"));
-      });
-      var hash = location.hash.substring(1);
-      if (hash.length)
-        setTimeout(function()
-        {
-          phoxy.SimpleApiRequest(hash);
-        }, 1000);
+      });      
     }
   );
 
 var phoxy =
 {
+  loaded : false,
+  Load : function( )
+  {
+	  this.loaded = true;
+	  var hash = location.hash.substring(1);
+      if (hash.length)
+          phoxy.SimpleApiRequest(hash);
+  }
+  ,
+  DeferRender : function (design, result, data)
+    {
+		setTimeout(function() {
+			phoxy.Render(design, result, data);
+		}, 10);
+    }
+  ,
+  Reset : function (url)
+  {
+	if (url == true || url == "true")
+	  location.reload();
+	var parts = url.split('#');
+	if (parts[1] == undefined)
+	  window.location.hash = '';
+	else
+	  window.location.hash = "#" + parts[1];
+	location.reload(parts[0]);
+  }
+  ,
   Render : function (design, result, data)
     {
       var source = design;
@@ -48,11 +70,11 @@ var phoxy =
       {
         alert(answer.error);
         if (answer.reset !== undefined)
-          location.reload(answer.reset);
+          phoxy.Reset(answer.reset);
         return;
       }
       if (answer.reset !== undefined)
-        location.reload(answer.reset);      
+        phoxy.Reset(answer.reset);      
       if (answer.script)
       {
         require(answer.script,
@@ -72,6 +94,8 @@ var phoxy =
         if (callback)
           callback(answer);
       }
+      if (!this.loaded)
+		  this.Load();
     }
   ,
   SimpleApiRequest : function( url )
@@ -81,5 +105,14 @@ var phoxy =
         $.getJSON(url, function(data) { phoxy.ApiAnswer(data); });
       });
     }
-  
+  ,
+  MenuCall : function( url )
+  {
+      $(function()
+      {
+        $.getJSON(url, function(data) {
+			window.location.hash = url;
+			phoxy.ApiAnswer(data); });
+      });	  
+  }
 }
