7c2fd1abf8c31f40f5bf663702cd37c9e13b33f8
Kirill Berezin <enelar@develop-project.ru>
2014-02-11 00:24:09 +0300

    Implemented minimal submodules support

--- index.php
+++ index.php
@@ -40,41 +40,12 @@ if (isset($_GET[$get_param]))
   $file = $_GET[$get_param];
   if ($file == 'htaccess')
     exit('Rewrite engine work SUCCESS');
-
-function ExportLast( $str )
-{
-  $method_delimeter_pos = strrpos($str, "/");
-  
-  if ($method_delimeter_pos > -1)
-  {
-    $func = substr($str, $method_delimeter_pos + 1);
-    $file = substr($str, 0, $method_delimeter_pos);
-  }
-  else
-    return array($str);
-  return array($file, $func);
-}
-  list($file, $func) = ExportLast($file);
-
-  
-  if (!isset($func) || !$func)
-    $func = 'Reserve';
     
-  session_start();
+  include_once('rpc_string_parser.php');
   
-  include_once('include.php');
-  
-  if ($file == 'phoxy') // reserved module name
-    $target_dir = realpath(dirname(__FILE__));
-  else
-    $target_dir = phoxy_conf()["api_dir"];
-    
-  list($subdir, $class) = ExportLast($file);
-  if (!isset($class))
-    list($subdir,$class) = array('',$subdir);
-  $a = IncludeModule($target_dir.'/'.$subdir, $class);
-  if (is_null($a))
-    exit(json_encode(array("error" => 'Undefined api handler required')));
+  $obj = GetRpcObject($file);
+  $a = $obj['obj'];
+  $func = $obj['method'];
 
   $get = $_GET;
   unset($get[$get_param]);
