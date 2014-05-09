329dd4a9a758c09e7ab63b2f5526c012c61f9fa8
Kirill Berezin <enelar@develop-project.ru>
2014-05-08 23:01:02 -0400

    EJS.View is deprecated

--- phoxy.js
+++ phoxy.js
@@ -5,8 +5,7 @@ requirejs.config({
 require([
   "libs/text", // part of require js
   "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js",
-  "libs/EJS/ejs.js",
-  "libs/EJS/view.js"
+  "libs/EJS/ejs.js"
   ],
     function(until)
     {
