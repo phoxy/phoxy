485f1e648541fab1344a76598db3caa3ecc4cfc5
Kirill Berezin <enelar@exsul.net>
2015-07-12 07:30:09 +0300

    Crossinclude without uopz extension

--- virtual_namespace_helper.php
+++ virtual_namespace_helper.php
@@ -2,19 +2,13 @@
 $code = file_get_contents($file);
 $obj = null;
 
-if (defined('tempns'))
-  if (!function_exists('uopz_undefine'))
-    die('You need uopz pecl extension for complex class cross includes');
-  else
-    uopz_undefine('tempns');
-
-define('tempns', 'tempns_'.md5(microtime()));
+$tempns = 'tempns_'.md5(microtime());
 //$new_source = preg_replace("/class(\s+)({$module})/", "class /".tempns."/$module", $code);
-$code = str_replace('<?php', 'namespace '.tempns.';', $code);
+$code = str_replace('<?php', 'namespace '.$tempns.';', $code);
 $code = preg_replace('/ api\s*\n/', '\\api', $code);
 
 eval($code);
 
-$classname = "\\".tempns."\\$module";
+$classname = "\\".$tempns."\\$module";
 $reflection = new ReflectionClass($classname);
 $obj = $reflection->newInstanceArgs($args);
