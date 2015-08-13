74f0970e4ba4b03c84414e8b6523817699080323
Kirill Berezin <enelar@exsul.net>
2015-08-13 10:45:34 +0300

    Since api::__construct is callable, catch protected exceptions from there

--- index.php
+++ index.php
@@ -19,19 +19,19 @@ function PhoxyStart()
   if ($file == 'htaccess')
     exit('Rewrite engine work SUCCESS');
 
-  include_once('rpc_string_parser.php');
-  $parser = new \phoxy\rpc_string_parser();
-
-  global $_phoxy_process_obj;
-  $_phoxy_process_obj = $obj = $parser->GetRpcObject($file, $_GET);
-  $a = $obj['obj'];
-
-  $method = $obj['method'];
-  if (is_string($method))
-    $method = [$method, []];
-
   try
   {
+    include_once('rpc_string_parser.php');
+    $parser = new \phoxy\rpc_string_parser();
+
+    global $_phoxy_process_obj;
+    $_phoxy_process_obj = $obj = $parser->GetRpcObject($file, $_GET);
+    $a = $obj['obj'];
+
+    $method = $obj['method'];
+    if (is_string($method))
+      $method = [$method, []];
+
     echo $a->APICall($method[0], $method[1]);
   } catch (phoxy_protected_call_error $e)
   {
