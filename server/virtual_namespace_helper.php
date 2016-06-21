2aab45134db2ef3b2ad8181038f6dc44b5a3b39f
Kirill Berezin <enelar@exsul.net>
2016-06-21 12:30:02 +0300

    Fix crossinclude conflict for phoxy class

--- server/virtual_namespace_helper.php
+++ server/virtual_namespace_helper.php
@@ -2,17 +2,21 @@
 $code = file_get_contents($file);
 $obj = null;
 
-$tempns = uniqid('phoxy\fork_');
+if ($classname != 'phoxy')
+{
+  $tempns = uniqid('phoxy\fork_');
 
-$header = <<<END
+  $header = <<<END
   namespace $tempns;
 
   use \phoxy as phoxy;
 END;
 
-$code = str_replace('<?php', $header, $code);
-$code = preg_replace('/ api\s*\n/', '\\api', $code);
+  $code = str_replace('<?php', $header, $code);
+  $code = preg_replace('/ api\s*\n/', ' \\api', $code);
 
-eval($code);
 
-$classname = "\\".$tempns."\\$module";
+  eval($code);
+
+  $classname = "\\".$tempns."\\$module";
+}
